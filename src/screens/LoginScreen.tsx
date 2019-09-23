e1cd767d0dcabaf4b30ba3751130aade89d6bce6
adshidtadka <nextage900fx@gmail.com>
2019-09-24 03:20:23 +0900

    ログイン画面に戻れないようにする

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, View, TextInput, Text, TouchableHighlight } from "react-native";
 import firebase from "firebase";
+import { StackActions, NavigationActions } from "react-navigation";
 
 class LoginScreen extends React.Component {
   state = {
@@ -14,9 +15,12 @@ class LoginScreen extends React.Component {
     firebase
       .auth()
       .signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then(user => {
-        console.log("success!", user);
-        this.props.navigation.navigate("Home");
+      .then(() => {
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [NavigationActions.navigate({ routeName: "Home" })]
+        });
+        this.props.navigation.dispatch(resetAction);
       })
       .catch(error => {
         console.log(error);
